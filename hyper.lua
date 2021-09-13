75d059485b71be0ae0cac56905439a571de58ec1
DookDekDEE <75850456+DookDekDEE@users.noreply.github.com>
2021-09-13 10:24:14 +0700

    Create hyper.lua

---
+++ hyper.lua
@@ -0,0 +1,3 @@
+local discordinv = "https://discord.gg/8FkZNVPCEs"
+game.Players.LocalPlayer:Kick("Join Discord Server for the updates! Invite link is copied. "..discordinv)
+setclipboard(discordinv)
