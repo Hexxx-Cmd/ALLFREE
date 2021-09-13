20d3b364dbd9d782ecc3907cee599cb507f69b2d
Hexxx <57963321+Hexxx-Cmd@users.noreply.github.com>
2021-09-13 18:54:36 +0100

    Delete hyper.lua

--- hyper.lua
+++
@@ -1,3 +0,0 @@
-local discordinv = "https://discord.gg/8FkZNVPCEs"
-game.Players.LocalPlayer:Kick("Join Discord Server for the updates! Invite link is copied. "..discordinv)
-setclipboard(discordinv)
